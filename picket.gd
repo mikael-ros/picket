9ab614c4b232b1fb06f510be4116e9dc4280096c
Mikael <112403418+mikael-ros@users.noreply.github.com>
2025-01-12 22:38:59 +0100

    Anchor implemented. Added custom setters and automatic property propagation

--- picket.gd
+++ picket.gd
@@ -5,53 +5,89 @@ extends TileMapLayer
 # -----------------------------
 # Properties used in the plugin
 @export_group("Texture positions")			# Positions in the tilemap for the textures used by the plugin
-@export var fence_texture_ID : int 	 = 0	## Position for the texture for the fence itself. Assumed to be first
-@export var fence_post_texture_ID : int = 1	## Position for the texture for the posts placed intermittently
+@export var fence_texture_ID : int = 0:		## Position for the texture for the fence itself. Assumed to be first
+	set(new_fence_texture_ID): 
+		fence_texture_ID = new_fence_texture_ID
+		redraw()
+@export var fence_post_texture_ID : int = 1:## Position for the texture for the posts placed intermittently
+	set(new_fence_post_texture_ID): 
+		fence_post_texture_ID = new_fence_post_texture_ID
+		redraw()
 
 @export_group("Positioning")				# Exported variables related to position of the fence
-@export_range(0,1, 0.1) var offset   = 0.0	## The offset
+@export_range(0,1, 0.1) var offset = 0.0:	## The offset
+	set(new_offset): 
+		offset = new_offset
+		set_properties()
+@export var origin_x : float = 0.0:			## Custom x origin position
+	set(new_x): 
+		origin_x = new_x
+		set_properties()
+@export var origin_y : float = 0.0:			## Custom y origin position
+	set(new_y): 
+		origin_y = new_y
+		set_properties()
 @export_subgroup("Anchor")					# Where in a tile the intersection of two fences will occur
-@export_range(0,1, 0.1) var anchor_x = 0.5	## Anchor x position
-@export_range(0,1, 0.1) var anchor_y = 0.5	## Anchor y position
+@export_range(0, 1, 0.01) var anchor_x : float = 0.5: ## Anchor x 
+	set(new_x): 
+		anchor_x = new_x
+		set_properties()
+@export_range(0, 1, 0.01) var anchor_y : float = 0.5: ## Anchor y 
+	set(new_y): 
+		anchor_y = new_y
+		set_properties()
 
 # ---------------
 # Private, local, properties
 var fence_layer_horizontal : TileMapLayer 	## The tile map layer upon which the horizontal parts are displayed
 var fence_layer_vertical : TileMapLayer 	## The tile map layer upon which the horizontal parts are displayed
 											# note: a post layer is not needed, as that's the "self" layer.
-var painted_cells : Array[Vector2i] 
-var added_cells : Array[Vector2i] 
-var removed_cells : Array[Vector2i] 
+var painted_cells : Array[Vector2i] 		## Used for tracking what cells were painted last revision
 # -----------------------------
 func _enter_tree() -> void:
 	# Initialize fence
 	fence_layer_horizontal = TileMapLayer.new()
 	fence_layer_vertical = TileMapLayer.new()
-	
-	# Copy certain properties over
-	fence_layer_horizontal.tile_set = tile_set
-	fence_layer_vertical.tile_set = tile_set
-	# todo: copy more properties over? maybe write a for-each? not sure. 
-	
-	# Set offset for fence layer
-	# This is simply the position + half a tile in the relevant direction
-	fence_layer_horizontal.position.x = position.x + tile_set.tile_size.x / 2
-	fence_layer_vertical.position.y = position.y + tile_set.tile_size.y / 2
+	set_properties()
 	
 	# Add children, draw initial fence
 	add_child(fence_layer_horizontal)
 	add_child(fence_layer_vertical)
 	
-	update_tiles()
+	update_tiles() # Update tiles first time
 
 func _exit_tree() -> void:
 	fence_layer_horizontal.free()
 	fence_layer_vertical.free()
 	
 func _ready() -> void:
-	set_process(Engine.is_editor_hint())
+	set_process(Engine.is_editor_hint()) # Only use [method _process] in the engine
+	if (Engine.is_editor_hint()):
+		changed.connect(set_properties)
 
 func _process(delta) -> void:
+	update_tiles() # Update tiles every tick. Not effecient, but cant find a better solution yet
+
+## Propagate properties over
+func set_properties() -> void:
+	# Copy certain properties over
+	if fence_layer_horizontal.tile_set != tile_set or fence_layer_vertical.tile_set != tile_set:
+		fence_layer_horizontal.tile_set = tile_set
+		fence_layer_vertical.tile_set = tile_set
+		redraw()
+	# todo: copy more properties over? maybe write a for-each? not sure. 
+	
+	# Adjust by anchor
+	position = Vector2(origin_x, origin_y) + Vector2(anchor_x * tile_set.tile_size.x, anchor_y * tile_set.tile_size.y)
+	
+	# Set offset for fence layer
+	# This is simply the position + half a tile in the relevant direction
+	fence_layer_horizontal.position.x = tile_set.tile_size.x / 2
+	fence_layer_vertical.position.y = tile_set.tile_size.y / 2
+	
+## Redraws all tiles. Used for texture changes
+func redraw() -> void:
+	painted_cells = []
 	update_tiles()
 	
 ## Update all tiles, based on differences
@@ -59,10 +95,12 @@ func update_tiles() -> void:
 	var prev_painted_cells = painted_cells
 	painted_cells = get_used_cells() 
 			
+	# Find and clear removed fences
 	for cell in prev_painted_cells:
 		if (not cell in painted_cells):
-			remove_fence_neighbors(cell)
+			clear_fence_neighbors(cell)
 			
+	# Find and draw new fences
 	for cell in painted_cells:
 		if (not cell in prev_painted_cells):
 			draw_fence_neighbors(cell)
@@ -86,7 +124,7 @@ func set_fence_cell(cell: Vector2i, neighbor: Vector2i) -> void:
 		fence_layer_horizontal.set_cell(pos, fence_texture_ID, Vector2i.ZERO)
 
 ## Remove neighbors of a certain cell.
-func remove_fence_neighbors(cell: Vector2i) -> void:
+func clear_fence_neighbors(cell: Vector2i) -> void:
 	for neighbor in get_surrounding_cells(cell): # Remove cells in every direction
 		fence_layer_horizontal.erase_cell(cell)							# Cell to the right
 		fence_layer_horizontal.erase_cell(Vector2(cell.x - 1, cell.y))	# Cell to the left
