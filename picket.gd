a517d073c452b888816f1c38fc43d4480ff91558
Mikael <112403418+mikael-ros@users.noreply.github.com>
2025-01-17 23:21:54 +0100

    Offsets almost working + refactoring

--- picket.gd
+++ picket.gd
@@ -51,8 +51,7 @@ var painted_cells : Array[Vector2i] 		## Used for tracking what cells were paint
 enum Axis {
 	HORIZONTAL,
 	VERTICAL,
-	BOTH,
-	NEITHER
+	BOTH_OR_NEITHER
 }
 	
 var inited = false
@@ -127,85 +126,43 @@ func update_tiles() -> void:
 			
 	# Find and clear removed fences
 	for cell in prev_painted_cells:
-		if (not cell in painted_cells):
+		if not cell in painted_cells:
 			clear_fence_neighbors(cell)
-			clear_post_cell(cell)
-			update_post_neighbors(cell)
+			clear_post_cell(cell, true)
 			
 	# Find and draw new fences
 	for cell in painted_cells:
-		if (not cell in prev_painted_cells):
+		if not cell in prev_painted_cells:
 			draw_fence_neighbors(cell)
-			draw_post_neighbors(cell)
-			update_post_neighbors(cell)
-			
+			draw_post_neighbors(cell, true)
 
+## Retrieve all neighbors that are drawn, as [method get_surrounding_cells] retrieves all possible neighbors, not only existing ones
+func get_drawn_neighbors(cell: Vector2i) -> Array[Vector2i]:
+	return get_surrounding_cells(cell).filter(func(c): return painted_cells.has(c))
 
+## Counts the amount of drawn neighbors in either direction
 func count_neighbors(cell: Vector2i) -> Vector2i:
-	var axis_neighbors = Vector2i.ZERO
-	for neighbor in get_surrounding_cells(cell): 	# For every neighbor cell
-		if painted_cells.has(neighbor):	 			# If neighbor is painted (get_surrounding_cells includes every cell, so)
-			if (neighbor.x == cell.x):
-				axis_neighbors.x += 1
-			else:
-				axis_neighbors.y += 1
+	var axis_neighbors = Vector2i.ZERO			# Neighbors on corresponding axis
+	for neighbor in get_drawn_neighbors(cell): 	# For every neighbor cell
+		if (neighbor.x == cell.x):				# If neighbor is on y-axis (vertical neighbor)
+			axis_neighbors.y += 1				# Increase y-axis neighbor count
+		else:									# Same, for x-axis (horizontal neighbor)
+			axis_neighbors.x += 1
 	return axis_neighbors
-	
-func determine_axis(axis_neighbors: Vector2i) -> Axis:
-	match axis_neighbors:
-		Vector2i(0,2):
-			return Axis.HORIZONTAL
+
+## Determines if cell is in a chain of strictly vertical or horizontal cells, or indeterminate
+func determine_axis(cell: Vector2i) -> Axis:
+	match count_neighbors(cell):
 		Vector2i(2,0):
+			return Axis.HORIZONTAL
+		Vector2i(0,2):
 			return Axis.VERTICAL
-		Vector2i(0,0):
-			return Axis.NEITHER
 		_:
-			return Axis.BOTH
+			return Axis.BOTH_OR_NEITHER
 
 ## Paints fence posts around and within a certain cell
-func draw_post_neighbors(cell: Vector2i) -> void:
-	var axis_neighbors = count_neighbors(cell)
-	var axis = determine_axis(axis_neighbors)
-	set_post_cell(cell, axis)
-					
-func update_post_neighbors(cell: Vector2i) -> void:
-	var all_neighbors = [Vector2i(cell.x-1,cell.y+1), Vector2i(cell.x-1,cell.y-1), Vector2i(cell.x+1,cell.y+1), Vector2i(cell.x+1,cell.y-1)]
-	all_neighbors.append_array(get_surrounding_cells(cell))
-	for neighbor in all_neighbors:
-		if painted_cells.has(neighbor):
-			clear_post_cell(neighbor)
-			draw_post_neighbors(neighbor)
-				
-## Paint neighbors of a certain cell. More performant than [method draw_fence]
-func draw_fence_neighbors(cell: Vector2i) -> void:
-	for neighbor in get_surrounding_cells(cell): 	# For every neighbor cell
-		if painted_cells.has(neighbor):	 			# If neighbor is painted (get_surrounding_cells includes every cell, so)
-			set_fence_cell(cell, neighbor)
-		
-
-## Paint connections between cells
-func set_fence_cell(cell: Vector2i, neighbor: Vector2i) -> void:
-	var pos = cell
-	if neighbor.x == cell.x: 	# If neighbor is on vertical axis
-		if neighbor.y < cell.y: # If neighbor is below
-			pos.y -= 1
-		fence_layer_vertical.set_cell(pos, fence_texture_ID, Vector2i.ZERO, TileSetAtlasSource.TRANSFORM_TRANSPOSE + TileSetAtlasSource.TRANSFORM_FLIP_V)
-	elif neighbor.y == cell.y:	# If neighbor is on horizontal axis
-		if neighbor.x < cell.x: # If neighbor is to the left
-			pos.x -= 1
-		fence_layer_horizontal.set_cell(pos, fence_texture_ID, Vector2i.ZERO)
-
-## Remove neighbors of a certain cell.
-func clear_fence_neighbors(cell: Vector2i) -> void:
-	for neighbor in get_surrounding_cells(cell): # Remove cells in every direction
-		fence_layer_horizontal.erase_cell(cell)							# Cell to the right
-		fence_layer_horizontal.erase_cell(Vector2(cell.x - 1, cell.y))	# Cell to the left
-		fence_layer_vertical.erase_cell(cell)							# Cell above
-		fence_layer_vertical.erase_cell(Vector2(cell.x, cell.y - 1))	# Cell below
-		
-## Paints posts in intermediary layers
-func set_post_cell(cell: Vector2i, axis: Axis) -> void:
-	match axis:
+func draw_post_neighbors(cell: Vector2i, new: bool = false) -> void:
+	match determine_axis(cell):
 		Axis.HORIZONTAL:
 			if offset > 0:
 				post_layer_horizontal.set_cell(Vector2(cell.x - 1, cell.y), fence_post_texture_ID, Vector2i.ZERO)
@@ -214,11 +171,44 @@ func set_post_cell(cell: Vector2i, axis: Axis) -> void:
 			if offset > 0:
 				post_layer_vertical.set_cell(Vector2(cell.x, cell.y - 1), fence_post_texture_ID, Vector2i.ZERO)
 			post_layer_vertical.set_cell(cell, fence_post_texture_ID, Vector2i.ZERO)
-		Axis.NEITHER, Axis.BOTH:
+		Axis.BOTH_OR_NEITHER:
 			post_layer_stationary.set_cell(cell, fence_post_texture_ID, Vector2i.ZERO)
+	if new:
+		update_post_neighbors(cell)
 
 ## Remove fence posts in a certain cell
-func clear_post_cell(cell: Vector2i) -> void:
+func clear_post_cell(cell: Vector2i, new: bool = false) -> void:
 	post_layer_horizontal.erase_cell(cell)
 	post_layer_vertical.erase_cell(cell)
 	post_layer_stationary.erase_cell(cell)
+	if new:
+		update_post_neighbors(cell)
+					
+func update_post_neighbors(cell: Vector2i) -> void:
+	var all_neighbors = [Vector2i(cell.x-1,cell.y+1), Vector2i(cell.x-1,cell.y-1), Vector2i(cell.x+1,cell.y+1), Vector2i(cell.x+1,cell.y-1)]
+	all_neighbors.append_array(get_drawn_neighbors(cell))
+	for neighbor in all_neighbors:
+		if painted_cells.has(neighbor):
+			clear_post_cell(neighbor)
+			draw_post_neighbors(neighbor)
+				
+## Paint neighbors of a certain cell. More performant than [method draw_fence]
+func draw_fence_neighbors(cell: Vector2i) -> void:
+	for neighbor in get_drawn_neighbors(cell): 	# For every neighbor cell
+		var pos = cell
+		if neighbor.x == cell.x: 	# If neighbor is on vertical axis
+			if neighbor.y < cell.y: # If neighbor is below
+				pos.y -= 1
+			fence_layer_vertical.set_cell(pos, fence_texture_ID, Vector2i.ZERO, TileSetAtlasSource.TRANSFORM_TRANSPOSE + TileSetAtlasSource.TRANSFORM_FLIP_V)
+		elif neighbor.y == cell.y:	# If neighbor is on horizontal axis
+			if neighbor.x < cell.x: # If neighbor is to the left
+				pos.x -= 1
+			fence_layer_horizontal.set_cell(pos, fence_texture_ID, Vector2i.ZERO)
+
+## Remove neighbors of a certain cell.
+func clear_fence_neighbors(cell: Vector2i) -> void:
+	fence_layer_horizontal.erase_cell(cell)							# Cell to the right
+	fence_layer_horizontal.erase_cell(Vector2(cell.x - 1, cell.y))	# Cell to the left
+	fence_layer_vertical.erase_cell(cell)							# Cell above
+	fence_layer_vertical.erase_cell(Vector2(cell.x, cell.y - 1))	# Cell below
+	
