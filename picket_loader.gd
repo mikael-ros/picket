bb3dc0bbf30a34e32e44fb90880d75ad08554f44
Mikael <112403418+mikael-ros@users.noreply.github.com>
2025-01-19 20:43:57 +0100

    Recolorized logo to fit Godot theme, changed dimensions, added icon to editor node

--- picket_loader.gd
+++ picket_loader.gd
@@ -1,9 +1,8 @@
 @tool
 extends EditorPlugin
 
-
 func _enter_tree():
-	add_custom_type("Picket", "TileMapLayer", preload("picket.gd"), preload("res://Textures/Lamp.png"))
+	add_custom_type("Picket", "TileMapLayer", preload("picket.gd"), preload("assets/picket_square_logo.svg"))
 
 
 func _exit_tree():
